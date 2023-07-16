f88c1f60560d55e4b34d2261b7b3a3d73ba95671
11yuuki-y <yuuuuuki20011234@icloud.com>
2023-07-16 17:57:00 +0900

    HPデザインコード

---
+++ resources/views/hp/index.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>M&Co.（エムアンドコー）：大分の美容室|</title>
+    <meta name="description" content="【大分駅徒歩3分/SNSで大人気】閑静な住宅街にあるプライベートサロン。
+    レディース メンズ、ジャンル問わず大歓迎！インスタ「@m_a_c_oita」スタイル写真や情報を要チェック">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minumum-scale=1">
+    <link rel="apple-touch-icon" href="webclip.png">
+    <meta property="og:site_name" content="M&Co.（エムアンドコー）：大分の美容室|">
+    <!-- <meta property="og:url" content="URL" /> -->
+    <meta property="og:type" content="website or article">
+    <meta property="og:title" content="M&Co.（エムアンドコー）：大分の美容室|">
+    <meta property="og:description" content="【大分駅徒歩3分/SNSで大人気】閑静な住宅街にあるプライベートサロン。
+    レディース メンズ、ジャンル問わず大歓迎！インスタ「@m_a_c_oita」スタイル写真や情報を要チェック">
+    <!-- <meta property="og:image" content="URL" /> -->
+    <meta property="og:locale" content="ja_JP">
+    
+</head>
+
+<body>
+    <div class="block">
+        <header>
+            <div class="header-list">
+                <div class="logo">
+                    <img src="{{ asset('img/logo.jpg') }}" class="logo-img">
+                </div>
+                <p class="logo-name">hair & nail salon</p>
+                <div class="page-link">
+                    <a href="#Style Gallery">Style Gallery</a>
+                    <a href="#Concept">Concept</a>
+                    <a href="#Menu">Menu</a>
+                    <a href="#Staff">Staff</a>
+                    <a href="#Nail">Nail</a>
+                    <a href="#Accese">Accese</a>
+                </div>
+            </div>
+            <div class="slider">
+                <div class="slider-list">
+                    <div class="slider1">
+                        <img src="">
+                    </div>
+                    <div class="slider2">
+                        <img src="">
+                    </div>
+                    <div class="slider3">
+                        <img src="">
+                    </div>
+                    <div class="slider4">
+                        <img src="">
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="right-side">
+            <div>
+                <img src="{{ asset('img/meny.png') }}" class="meny-img">
+                <p class="reservation">>Reservation</p>
+            </div>
+            <p class="copy-right">&copy; ©︎ 2023 hair salon M&Co.</p>
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\indexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', [indexController::class, 'index']);
+
+
+// トップページ
+Route::get('/hp', [indexController::class, 'index'])->name('hp.index');
+
