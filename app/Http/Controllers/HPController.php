1f5d25f23887fbfd53aef2bdc9839c838b7a7a18
11yuuki-y <yuuuuuki20011234@icloud.com>
2023-08-05 16:57:51 +0900

    マイグレーションファイル・コントrーラーの作成

--- app/Http/Controllers/HPController.php
+++ app/Http/Controllers/HPController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HPController extends Controller
+class HpController extends Controller
 {
     // トップページ
     public function index()
--- resources/views/admin/registration/index.blade.php
+++ resources/views/admin/registration/index.blade.php
@@ -1,20 +1,25 @@
 @extends('admin.app')
 
 @section('vite')
-@vite(['resources/scss/admin/register/index.scss'])
+@vite(['resources/scss/admin/registration/index.scss'])
 @endsection
 
 @section('content')
 <main>
     <div class="wrapper">
         <div class="back">
-            <a href="{{ route('admin.list.index') }}">&lt; 戻る</a>
+            <a href="{{ route('admin.header.index') }}">&lt; 戻る</a>
         </div>
-        <div class="file">
-            <label for="img">画像</label>
-            <input type="file" name="img" accept="image/*">
-        </div>
-        <p class="register"><a href="#">登録</a></p>
+        <form action="#" method="post">
+            @csrf
+            <div class="file">
+                <label for="img">画像</label>
+                <input type="file" name="img" accept=".jpg,.jpeg,.png,.gif,.svg" required>
+            </div>
+            <div class="button">
+                <button type="submit">登録</button>
+            </div>
+        </form>
     </div>
 </main>
 @endsection
